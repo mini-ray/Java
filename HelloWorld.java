9314c764bda167e3c9071064d540d9e82943873e
mini-ray <55558121+mini-ray@users.noreply.github.com>
2022-01-18 11:25:00 -0800

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,10 @@
+//The classic Hello World program!
+class HelloWorld {
+
+	public static void main(String[] args) {
+	
+		//write Hello World to the terminal window
+		System.out.println("Hello World!");
+	
+	}
+}
